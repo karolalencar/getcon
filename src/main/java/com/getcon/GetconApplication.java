40fd92e685902e47a96e61025ee5319dd17388cd
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2024-03-06 19:35:11 -0300

    initial commit

---
+++ src/main/java/com/getcon/GetconApplication.java
@@ -0,0 +1,13 @@
+package com.getcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getcon/GetconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
